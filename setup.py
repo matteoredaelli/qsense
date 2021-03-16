bfa0dc516d0879af431e080f520ab06c5780c0d3
Matteo Redaelli <matteo.redaelli@gmail.com>
2021-03-16 22:24:31 +0100

    added license headers with addlicense

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
+# Copyright (c) 2021 Matteo Redaelli
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import setuptools
 
 with open("README.md", "r") as fh:
@@ -5,23 +20,22 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="qsense",
-    version="0.3.1",
+    version="0.4.0",
     author="Matteo Redaelli",
     author_email="matteo.redaelli@gmail.com",
-    description="qsense is a python library and command line tool for qliksense",
+    description="qsense is a python library and a command line tool for qliksense",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/matteoredaelli/qsense",
     packages=setuptools.find_packages(),
     license="GPL",
-    entry_points = {
-        'console_scripts': ['qsense=qsense.command_line:main'],
+    entry_points={
+        "console_scripts": ["qsense=qsense.command_line:main"],
     },
-
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    install_requires=['requests', 'requests_ntlm', 'qsapi', 'fire'],
-    python_requires='>=3.6',
+    install_requires=["requests", "requests_ntlm", "qsapi", "fire"],
+    python_requires=">=3.6",
 )
